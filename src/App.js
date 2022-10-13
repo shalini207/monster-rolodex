d09a504c40cb6a8a10cd461e0ca5537a8cbf6b16
shalini <shalinitcs9@gmail.com>
2022-10-13 20:39:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.components";
+import { SearchBox } from "./components/search-box/search-box.components";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+    // this.handleChange = this.handleChange.bind(this);     := when we dont use arrow function on handlechange then we bind this method
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
